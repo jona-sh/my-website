de1d17ea92a117897774d98a713c2e8c2fe8f50c
jona-sh <jonas-hoepker@proton.me>
2026-09-20 20:38:00 +0200

    home icon

--- layout.js
+++ layout.js
@@ -3,11 +3,18 @@ const prefix = inPagesFolder ? "../" : "";
 
 document.querySelector("header").innerHTML = `
     <ul class="horizontaleAnordnung">
+        <li>
+            <a class="home-button" href="${prefix}index.html" aria-label="Startseite" title="Startseite">
+                <svg viewBox="0 0 24 24" aria-hidden="true">
+                    <path d="M3 10.5 12 3l9 7.5v9a1.5 1.5 0 0 1-1.5 1.5h-15A1.5 1.5 0 0 1 3 19.5v-9Z"/>
+                    <path d="M9 21v-6h6v6"/>
+                </svg>
+            </a>
+        </li>
         <li><a href="${prefix}pages/formats.html">erster Eintrag</a></li>
         <li>zweiter Eintrag</li>
         <li>Liste innerhalb des Headers</li>
         <li>also formatiert</li>
-        <li><a href="${prefix}index.html">Startseite</a></li>
     </ul>
 `;
 
@@ -15,7 +22,7 @@ document.querySelector("footer").innerHTML = `
     <p>&copy; 2026 Meine Website</p>
     <nav>
         <a href="https://github.com/jona-sh">Kontakt</a>
-        <a href="${prefix}impressum.html">Impressum</a>
-        <a href="${prefix}datenschutz.html">Datenschutz</a>
+        <a href="${prefix}pages/impressum.html">Impressum</a>
+        <a href="${prefix}pages/datenschutz.html">Datenschutz</a>
     </nav>
 `;
